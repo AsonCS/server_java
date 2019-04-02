c607340491d5a8bd1fb7cfbeeb58fcd914b3b828
Anderson Costa da Silva <42609750+AsonCS@users.noreply.github.com>
2019-04-02 18:30:30 -0300

    Update Server.java

--- Server.java
+++ Server.java
@@ -15,36 +15,54 @@ public class Server {
         this.routes = new ArrayList<>();
     }
     
-    private TreeMap<String, Object> processRoute(String route){
-        TreeMap<String, Object> objects = new TreeMap<>();
-        TreeMap<String, String> params = new TreeMap<>();
-        String response = "";
+    private String[] processRoute(String route){
+        String[] response = new String[2];
         if(route.split("/").length < 1){
-            objects.put("route", response);
-            objects.put("params", params);
-            return objects;
+        	response[0] = "";
+            response[1] = "";
+            return response;
         }
         for(String b : route.split("/")){
             if(!b.contains("{") && !b.contains("}")){
-                response = b.length() > 0 ? response + "/" + b : response + "";
+                response[0] = b.length() > 0 ? response[0] + "/" + b : response[0];
+                response[1] = b.length() > 0 ? response[1] + "/?" : response[1];
             }else{
-                response = b.length() > 0 ? response + "/([^/]+)" : response + "";
-                params.put(b.substring(b.indexOf("{") + 1, b.indexOf("}")), "");
+                response[0] = b.length() > 0 ? response[0] + "/([^/]+)" : response[0];
+                response[1] = b.length() > 0 ? response[1] + "/" + b.substring(b.indexOf("{") + 1, b.indexOf("}"))  : response[1];
             }
         }
-        //System.out.println(url.matches(this.route));
-        objects.put("route", response);
-        objects.put("params", params);
-        return objects;
+        return response;
     }
     
     public Server addRoute(String route, Handler handler, Parameter ...parameters){
-        TreeMap<String, Object> map = processRoute(route);
-        route = (String) map.get("route");
-        TreeMap<String, String> params = (TreeMap<String, String>) map.get("params");
-        routes.add(new Route(route, handler, parameters, params));
+        String[] map = processRoute(route);
+        routes.add(new Route(map[0], map[1], handler, parameters));
         return this;
     }
+    
+    public Server removeRoute(int idx){
+        if(idx > -1 && idx < routes.size()) routes.remove(idx);
+        return this;
+    }
+    
+    public void start() throws Exception{
+        if(routes.size() > 0){
+            ServerSocket servidor = new ServerSocket(8080);
+            System.out.println("Servidor ouvindo a porta 8080");
+            while (true) {
+                try{
+                    Socket cliente = servidor.accept();
+                    cliente.setSoTimeout(10000);                
+                    Thread t = new Thread(new ServerContext(cliente, routes));
+                    t.start();
+                } catch (Exception ex) {
+                    System.err.println(ex.getMessage());
+                }
+            }
+        }
+    }
+    
+}
     
     public Server removeRoute(int idx){
         if(idx > -1 && idx < routes.size()) routes.remove(idx);
